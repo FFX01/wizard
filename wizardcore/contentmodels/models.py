cbe042f083194833cb6899a43949cd09460a587f
ffx01 <walters.justin01@gmail.com>
2016-03-06 14:32:51 -0800

    V0.0.2 - Added documentation to contentmodels/models

--- wizardcore/contentmodels/models.py
+++ wizardcore/contentmodels/models.py
@@ -4,7 +4,22 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 
 class BaseAbstractContentObject(models.Model):
+    """
+    All content models inherit from this abstract class. This is a non
+    hierarchical model.
 
+    :ivar title: Used for verbose reference, such as in the admin panel.
+    :type title: basestring
+
+    :ivar slug: Used for reference or for generating paths.
+    :type slug: basestring
+
+    :ivar seo_title: Used for search engine optimization when needed.
+    :type seo_title: basestring
+
+    :ivar seo_description: Used for search engine optimization when needed.
+    :type seo_description: basestring
+    """
     title = models.CharField(
         blank=False,
         max_length=120
@@ -26,18 +41,37 @@ class BaseAbstractContentObject(models.Model):
         abstract = True
 
     def save(self, *args, **kwargs):
+        """
+        Automatically generates a slug if a slug is not defined.
+
+        :param args: Extra positional arguments passed to the save method.
+
+        :param kwargs: Extra keyword arguments passed to the save method.
+
+        :returns Passes the instance back to the original overridden save
+        method.
+        """
         if not self.slug:
             self.slug = slugify(self.title)
         super(BaseAbstractContentObject, self).save()
 
 
 class ContentObject(BaseAbstractContentObject):
-
+    """
+    Basic non-hierarchical content object.
+    """
     class Meta:
         abstract = True
 
 
 class NestedContentObject(BaseAbstractContentObject, MPTTModel):
+    """
+    Basic hierarchical content object. Used for nested objects.
+
+    :ivar parent: A model instance's parent object. Not required for top level
+    model instances.
+    :type parent: object
+    """
 
     parent = TreeForeignKey(
         'self',
